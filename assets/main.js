c704d38b5aefeb61b80347f02d6fbfb231e4813b
Riccardo <riky.volto@live.com>
2023-09-07 14:52:35 +0200

    put all images in a array

--- assets/main.js
+++ assets/main.js
@@ -54,11 +54,22 @@ Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella mile
 
 /* Define the slides list */
 const slides = [
-    './assets/img/01.webp', //0
-    './assets/img/02.webp', //1
-    './assets/img/03.webp', //etc
-    './assets/img/04.webp',
-    './assets/img/05.webp',
+    {
+        immagine : './assets/img/01.webp',
+    },
+    {
+        immagine : './assets/img/05.webp',
+    },
+    {
+        immagine : './assets/img/04.webp',
+    },
+    {
+        immagine : './assets/img/03.webp',
+    },
+    {
+        immagine : './assets/img/02.webp',
+    },
+
   ]
   
   let activeSlide = 0;
@@ -223,3 +234,16 @@ const slides = [
     // add the active class to the next slide
     nextSlide.classList.add('active')
   })
+
+
+  /* Consegna:
+Riprendiamo il live coding visto in classe un pó di tempo fá sul carosello di immagini e rifacciamolo :gatto_che_urla:, questa volta usando gli oggetti.
+:boolean-hug: Potete prendere come riferimento il codice scritto insieme nel live, che troverete direttamente nella mia repository di github a questo link: [https://github.com/fabiopacifici/104_js/tree/main/live_slider]
+Modifichiamo il codice dell'esercizio per renderlo funzionante con un array di oggetti al posto di un array di stringhe.
+Bonus 0:
+Non eramamo ancora a conoscenda di molti strumenti utili, come ad esempio le funzioni. É possibile fare refactoring del codice, pulendolo e creando quanche funzione che possa rendere tutto piú leggibile e pulito?
+Bonus 1:
+Sperimentiamo attraverso l'uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:
+al click di un bottone o già dall'inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
+Bonus 2:
+E se volessi un bottone per invertire la "direzione" del carosello? */
